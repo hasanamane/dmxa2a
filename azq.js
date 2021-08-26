4bcbe752c10479dcd8cf65c573e8d8eb41d09a5e
hasanamane <89529514+hasanamane@users.noreply.github.com>
2021-08-26 11:56:08 +0300

    Add files via upload

---
+++ azq.js
@@ -0,0 +1,10 @@
+function openNav()
+{
+    document.getElementById("myasidenav").style.width="200px";
+    document.getElementById("main").style.width="200px";
+}
+function closeNav()
+{
+    document.getElementById("myasidenav").style.width="0px";
+    document.getElementById("main").style.width="0px";
+}
